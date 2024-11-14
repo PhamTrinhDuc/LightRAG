cb99e1f9543876e65725ff7a08fbf16ebe88579a
PhamTrinhDuc <duc78240@gmail.com>
2024-11-14 17:58:27 +0700

    update code

--- src/llms.py
+++ src/llms.py
@@ -34,7 +34,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 @retry(
     stop=stop_after_attempt(3), # dừng sau 3 lần thử
-    wait=wait_exponential(multiplier=1, min=2, max=10), # thời gian chờ giữa các lần thửtăng theo cấp số nhân
+    wait=wait_exponential(multiplier=1, min=4, max=10), # thời gian chờ giữa các lần thửtăng theo cấp số nhân
     retry=retry_if_exception_type(RateLimitError, APIConnectionError, Timeout) # thử lại nếu gặp lỗi RateLimitError, APIConnectionError, Timeout
 )
 async def openai_complete_if_cache(
@@ -68,7 +68,6 @@ async def openai_complete_if_cache(
         AsyncOpenAI() if not base_url else AsyncOpenAI(base_url=base_url)
     )
 
-    hashing_kv: BaseKVStorage = kwargs.get("hashing_kv", None)
     messages = []
     if system_prompt:
         messages.append({"role": "system", 
@@ -77,15 +76,188 @@ async def openai_complete_if_cache(
     messages.append({"role": "user",
                     "content": prompt})
     
+
+    hashing_kv: BaseKVStorage = kwargs.get("hashing_kv", None)
     if hashing_kv is not None:
-        args_hash = compute_args_has(model, messages)
-        if_cache_return  = await hashing_kv.get_by_id(args_hash)
+        id_hash = compute_args_has(model, messages)
+        if_cache_return  = await hashing_kv.get_by_id(id_hash)
         if if_cache_return is not None:
-            return if_cache_return['return']
+            return if_cache_return['response']
         
     response = await openai_async_client.chat.completions.create(
         model=model,
         messages=messages,
         **kwargs
     )
+
+    if hashing_kv is not None:
+        await hashing_kv.upsert(
+            {id_hash: {"response": response.choices[0].message.content, "model": model}}
+        )
     return response.choices[0].message.content
+
+
+@retry(
+    stop=stop_after_attempt(3),
+    wait=wait_exponential(multiplier=1, min=4, max=10),
+    retry=retry_if_exception_type(RateLimitError, APIConnectionError, Timeout)
+)
+async def aruze_openai_complete_if_cache(
+    model,
+    prompt,
+    system_prompt: str = None,
+    history_messages: List = [],
+    base_url: str = None,
+    api_key: str = None,
+    **kwargs,
+) -> str:
+    
+    if api_key:
+        os.getenv["AZURE_OPENAI_API_KEY"] = api_key
+    if base_url:
+        os.environ["ARUZE_OPENAI_ENDPOINT"] = base_url
+
+    openai_async_client = AsyncAzureOpenAI(
+        azure_endpoint=os.getenv("ARUZE_OPENAI_ENDPOINT"),
+        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+        api_version=os.getenv("AZURE_OPENAI_API_VERSION")
+    )
+
+    messages = []
+    messages.append({"role": "system", 
+                     "content": system_prompt})
+    messages.extend(history_messages)
+    messages.append({"role": "user",
+                     "content": prompt})
+    
+    hashing_kv: BaseKVStorage = kwargs.get("hashing_kv", None)
+    if hashing_kv is not None:
+        id_hash = compute_args_has(model, messages)
+        if_return_cache = hashing_kv.get_by_id(id_hash)
+        if if_return_cache is not None:
+            return if_return_cache['response']
+    
+    response = openai_async_client.chat.completions.create(
+        model=model,
+        messages=messages,
+        **kwargs
+    )
+
+    if hashing_kv is not None:
+        await hashing_kv.upsert(
+            {id_hash: {"response": response.choices[0].message.content, "model": model}}
+        )
+    
+    return response.choices[0].message.content
+
+
+@lru_cache(maxsize=1)
+async def hf_model_if_cache(
+    model,
+    prompt,
+    system_prompt: str = None,
+    history_message: List = [],
+    **kwargs
+) -> str:
+    
+    hf_tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=model,
+        device_map = "auto",
+        trust_remote_code = True)
+    hf_model = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=model,
+        device_map = "auto",
+        trust_remote_code = True
+    )
+
+    if hf_tokenizer.pad_token is None:
+        hf_tokenizer.pad_token = hf_tokenizer.eos_token
+
+    messages =[]
+    messages.append({"role": "system",
+                     "content": system_prompt})
+    messages.extend(history_message)
+    messages.append({"role": "user",
+                     "content": prompt})
+    
+    hashing_kv: BaseKVStorage = kwargs.get("hashing_kv", None)
+    if hashing_kv is not None:
+        id_hash = compute_args_has(model, messages)
+        if_cache_return = hashing_kv.get_by_id(id_hash)
+        if if_cache_return is not None:
+            return if_cache_return['response']
+    
+    input_prompt = ""
+    try: 
+        input_prompt = hf_tokenizer.apply_chat_template(
+            conversation=messages, 
+            tokenize=False, 
+            add_generation_prompt=True
+        )
+    except Exception:
+        try:
+            ori_message = copy.deepcopy(messages)
+            if messages[0]['role'] == 'system':
+                messages[1]['content'] = (
+                    "<system>" + messages[0]['content'] + 
+                    "</system>\n" + messages[1]['content']
+                )
+                messages = messages[1:]
+                input_prompt = hf_tokenizer.apply_chat_template(
+                    conversation=messages,
+                    tokenize=False, 
+                    add_generation_prompt=True
+                )
+        except Exception:
+            len_message = len(ori_message)
+            for mgid in range(len_message):
+                input_prompt = (
+                    input_prompt 
+                    + "<" + ori_message[mgid]['role'] + ">" 
+                    + "<" + ori_message[mgid]['content'] + ">\n"
+                )
+    
+    input_ids = hf_tokenizer(
+        text=input_prompt,
+        return_tensors="pt",
+        padding=True,
+        truncation=True
+    ).to("cuda")
+    inputs = {k: v.to(hf_model.device) for k, v in input_ids.items()}
+    output = hf_model.generate(**inputs, max_new_tokens=512, num_return_sequences=1, early_stopping=True)
+
+    response_text = hf_tokenizer.decode(
+        output[0][len(inputs['input_ids'][0]): ], skip_special_tokens=True
+    )
+
+    if hashing_kv is not None:
+        await hashing_kv.upsert(
+            {id_hash: {"response": response_text, "model": model}}
+        )
+    return response_text
+
+
+async def ollama_model_if_cache(
+    model, 
+    prompt, 
+    system_prompt: str = None,
+    history_messages: List = [],
+    **kwargs
+) -> str:
+    kwargs.pop("max_token", None)
+    kwargs.pop("resposne_format", None)
+    host = kwargs.pop("host", None)
+    timeout = kwargs.pop("timeout", None)
+    ollama_client = ollama.AsyncClient(host=host, timeout=timeout)
+
+    messages = []
+    if system_prompt:
+        messages.append({"role": "system", 
+                         "content": system_prompt})
+    
+    messages.extend(history_messages)
+    messages.append({"role": "user", 
+                     "content": prompt})
+    
+    hashing_kv: BaseKVStorage = kwargs.pop("hashing_kv", None)
+    
