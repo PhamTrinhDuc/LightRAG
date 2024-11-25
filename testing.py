a6e6dbfd2e9f1b2fdd11681d32cad2a1fa21b795
PhamTrinhDuc <duc78240@gmail.com>
2024-11-25 13:49:15 +0700

    update code

--- testing.py
+++ testing.py
@@ -1,14 +1,22 @@
-from src.llms import openai_complete_if_cache
+from src.llms import openai_complete_if_cache, openai_embedding
 import os
 import asyncio
 
 
 async def main():
-    response = await openai_complete_if_cache(
-        model='gpt-4o-mini',
-        prompt='What is the capital of France?',
-    )
-    return response
+    # test async function openai gpt4o-mini
+    # response = await openai_complete_if_cache(
+    #     model='gpt-4o-mini',
+    #     prompt='What is the capital of France?',
+    # )
+    # print(response) => text response
+    #=====================================================================================
+    # test async function openai embedding  
+    response = await openai_embedding(texts = ["hello", "hi"])
+    print(response.shape) # => shape = (2, 1536)
 
-res = asyncio.run(main())
-print(res)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
